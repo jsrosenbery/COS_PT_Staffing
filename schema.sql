def33741fa9c960791a0d23dccb398123138d1e1
jsrosenbery <jacoba@cos.edu>
2026-04-11 22:57:18 -0700

    Delete schema.sql

--- schema.sql
+++
@@ -1,9 +0,0 @@
-import pkg from "pg";
-const { Pool } = pkg;
-
-export const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
-});
-
-export const query = (text, params) => pool.query(text, params);
